6e65960184de54f95726c82d38ff18a8c910f554
Chakshan <chakshan.kothakota@gmail.com>
2024-02-14 11:53:05 -0800

    initial commit

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,21 +1,48 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class IsMoon implements StringChecker {
-  public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
-  }
+    public boolean checkString(String s) {
+        return s.equalsIgnoreCase("moon");
+    }
+}
+
+class StringContains implements StringChecker {
+    public boolean checkString(String s) {
+        return s.contains("a");
+    }
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
+    @Test(timeout = 500)
+        public void testMergeRightEnd() {
+        List<String> left = Arrays.asList("a", "b", "c");
+        List<String> right = Arrays.asList("a", "d");
+        List<String> merged = ListExamples.merge(left, right);
+        List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+        assertEquals(expected, merged);
+    }
+
+    @Test
+    public void testFilterMultipleMatch() {
+        ArrayList<String> input = new ArrayList<>();
+        input.add("a");
+        input.add("ab");
+        input.add("abc");
+        input.add("bcd");
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a");
+        expected.add("ab");
+        expected.add("abc");
+
+        StringContains contains = new StringContains();
+        List<String> output = ListExamples.filter(input, contains);
+        assertEquals(expected, output);
+    }
 }
--- grade.sh
+++ grade.sh
@@ -5,7 +5,7 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission 2> git-output.txt
 echo 'Finished cloning'
 
 
@@ -14,3 +14,57 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+find student-submission -name "*.java" -exec cp {} grading-area \;
+# cp student-submission/*.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ]
+then
+	echo "Missing ListExamples.java. Did you forget the file or misname it?"
+	echo "Score: 0"
+	exit 1
+fi 
+
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]
+then 
+	echo "Could not compile. See compiler errors above."
+	echo "Score: 0"
+	exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+
+RESULT_LINE=$(grep "Tests run:" test-output.txt)
+
+if [ -n "$RESULT_LINE" ]
+then
+	TESTS_RUN=$(echo $RESULT_LINE | awk -F'[, ]' '{ print $3 }')
+	FAILURES=$(echo $RESULT_LINE | awk -F'[, ]' '{ print $6 }')
+	SUCCESSES=$((TESTS_RUN-FAILURES))
+
+	SCORE=$(( 100 * $SUCCESSES / $TESTS_RUN ))
+
+	echo "Passed $SUCCESSES out of $TESTS_RUN tests"
+	echo "Score: $SCORE"
+	exit 0
+fi
+
+RESULT_LINE=$(grep "OK (" test-output.txt)
+if [ -n "$RESULT_LINE" ]
+then
+	TESTS_RUN=$(echo $RESULT_LINE | awk -F'[( ]' '{ print $3 }')
+	echo "Passed $TESTS_RUN out of $TESTS_RUN tests"
+	echo "Score: 100"
+	exit 0
+fi
+
+echo "Error. Invalid tests output."
+exit 1
+
+
